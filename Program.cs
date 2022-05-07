8f6d66ffd4f48ca6e0206dd0f7c78512aea52856
Mikhail Vinogradov <m89119750476@gmail.com>
2022-05-07 03:16:23 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int x = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        x++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше нуля: {x}");
